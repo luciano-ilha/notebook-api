99866f9ad6afef03ca2c2a701e0f29f6abddbe9e
Luciano Ilha <ilha.dev.br@gmail.com>
2021-06-14 09:15:17 -0300

    Add pry gem to gemfilea and change as_json to format dates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,9 +2,17 @@ class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
 
-  def birthdate_br
-    I18n.l(self.birthdate) unless self.birthdate.blank?
+  accepts_nested_attributes_for :phones, allow_destroy: true
+
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
   end
+
+  # def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
 
   # def author
   #   "Luciano Ilha"
